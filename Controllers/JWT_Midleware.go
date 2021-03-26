ca291bfd6fecc8a23a4d590b64a967027fbb22b9
duongnhathuy99 <55747198+duongnhathuy99@users.noreply.github.com>
2021-03-26 15:19:48 +0700

    abc

--- Controllers/JWT_Midleware.go
+++ Controllers/JWT_Midleware.go
@@ -1,26 +1,24 @@
 package Controllers
 
 import (
-	jwtmiddleware "github.com/auth0/go-jwt-middleware"
-	"github.com/dgrijalva/jwt-go"
 	"io"
-	"main.go/Database"
 	"net/http"
 )
 
 //JWT authorization middleware
-func AuthMW(next http.Handler) http.Handler {
-
-	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
-		Extractor: jwtmiddleware.FromFirst(jwtmiddleware.FromAuthHeader,
-			jwtmiddleware.FromParameter("token")),
-		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-			return []byte(Database.APP_KEY), nil
-		},
-		SigningMethod: jwt.SigningMethodHS256,
-	})
-	return jwtMiddleware.Handler(next)
-}
+//func AuthMW(next http.Handler) http.Handler {
+//
+//jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+//	Extractor: jwtmiddleware.FromFirst(jwtmiddleware.FromAuthHeader,
+//		jwtmiddleware.FromParameter("token")),
+//	//ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+//		return []byte(Database.APP_KEY), nil
+//	},
+//	SigningMethod: jwt.SigningMethodHS256,
+//})
+//return jwtMiddleware.Handler(next)
+//retu
+//}
 
 //Validate Token
 func ValidateToken(w http.ResponseWriter, r *http.Request) {
