120d334851407243d8ead4be34a8977b141ebce0
thanhhuydev77 <thanhhuydev77@gmail.com>
2021-06-22 21:59:55 +0700

    fix read config

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
 	"fmt"
 	"github.com/rs/cors"
+	"io/ioutil"
 	"log"
 	"main.go/Controllers"
 	"main.go/Database"
 	"main.go/Models"
 	"net/http"
-	"strconv"
+	"os"
 )
 
 func main() {
@@ -25,50 +26,51 @@ func main() {
 }
 
 //heroku
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "8001"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
+}
+
 //func GetPort() string {
-//	var port = os.Getenv("PORT")
+//	port := flag.Int("port", -1, "specify a port")
 //	// Set a default port if there is nothing in the environment
-//	if port == "" {
-//		port = "8001"
-//		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+//	if *port == -1 {
+//		*port = 8001
+//		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + strconv.Itoa(*port))
 //	}
-//	return ":" + port
+//	return ":" + strconv.Itoa(*port)
 //}
 
-func GetPort() string {
-	port := flag.Int("port", -1, "specify a port")
-	// Set a default port if there is nothing in the environment
-	if *port == -1 {
-		*port = 8001
-		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + strconv.Itoa(*port))
+func ReadConfigfile() {
+	jsonFile, err := os.Open("Config/AppConfig.text")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
 	}
-	return ":" + strconv.Itoa(*port)
+	fmt.Println("Opened successfully")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	json.Unmarshal((byteValue), &Models.AppConfig)
+	log.Print("Read Config successfully!")
 }
 
 //func ReadConfigfile() {
-//	jsonFile, err := os.Open("Config/AppConfig.text")
+//	//jsonFile, err := os.Open("Config/AppConfig.text")
 //	// if we os.Open returns an error then handle it
-//	if err != nil {
-//		fmt.Println(err)
-//	}
-//	fmt.Println("Opened successfully")
-//	// defer the closing of our jsonFile so that we can parse it later on
-//	defer jsonFile.Close()
-//
-//	byteValue, _ := ioutil.ReadAll(jsonFile)
-//
-//	json.Unmarshal((byteValue), &Models.AppConfig)
+//	a := new(Models.Config)
+//	a.HostMailPassword = "Thanhhuyd71t9"
+//	a.HostMail = "errormonitoringvn@gmail.com"
+//	a.AppKey = "thisissecreckeyyesitisreallyofcourcetrustmeitiskeyofthisapphahaha"
+//	a.DBConnectionURL = "mongodb+srv://hathanhhuy:Thanhhuyd71t9@mycluster.5dvo9.mongodb.net/test?authSource=admin&replicaSet=atlas-f8f9l2-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+//	//json.Unmarshal((byteValue), &Models.AppConfig)
+//	Models.AppConfig = a
 //	log.Print("Read Config successfully!")
 //}
-func ReadConfigfile() {
-	//jsonFile, err := os.Open("Config/AppConfig.text")
-	// if we os.Open returns an error then handle it
-	a := new(Models.Config)
-	a.HostMailPassword = "Thanhhuyd71t9"
-	a.HostMail = "errormonitoringvn@gmail.com"
-	a.AppKey = "thisissecreckeyyesitisreallyofcourcetrustmeitiskeyofthisapphahaha"
-	a.DBConnectionURL = "mongodb+srv://hathanhhuy:Thanhhuyd71t9@mycluster.5dvo9.mongodb.net/test?authSource=admin&replicaSet=atlas-f8f9l2-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
-	//json.Unmarshal((byteValue), &Models.AppConfig)
-	Models.AppConfig = a
-	log.Print("Read Config successfully!")
-}
